98a2a4286e93401d6663e88d987fb865d2a3d43f
Cody Lopez <codylopez@Macs-MacBook.local>
2012-08-14 17:57:06 -0700

    password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+	http_basic_authenticate_with :name => "mozapolis", :password => "mozapolis88"
 
 	rescue_from CanCan::AccessDenied do |exception|
 		redirect_to restricted_path, :alert => "Hey, what are you up too?"
