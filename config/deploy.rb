6bf0ed146fdf6261398fae6e33be682800b705c1
Cody Lopez <cody@hellcod.es>
2012-08-31 17:33:22 -0700

    lots of updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,21 +7,34 @@ load "config/recipes/postgresql"
 load "config/recipes/nodejs"
 load "config/recipes/rbenv"
 load "config/recipes/check"
-load 'deploy/assets'
+load "deploy/assets"
 
-server "mozapolis.justclickmedia.com", :web, :app, :db, primary: true
+server "184.72.41.252", :web, :app, :db, primary: true
 
-set :user, "cody"
-set :application, "moza1"
+set :user, "ubuntu"
+set :application, "moza"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:hellocodes/moza.git"
+set :repository, "git@github.com:hellocodes/#{application}.git"
 set :branch, "master"
+
+set :maintenance_template_path, File.expand_path("../recipes/templates/maintenance.html.erb", __FILE__)
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "devmoz.pem")]
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+# To setup new Ubuntu 12.04 server:
+# ssh root@xxxxxxxxxx
+# adduser deployer
+# echo "deployer ALL=(ALL:ALL) ALL" >> /etc/sudoers
+# exit
+# ssh-copy-id deployer@xxxxxxxxxx
+# cap deploy:install
+# cap deploy:setup
+# cap deploy:cold
--- config/recipes/base.rb
+++ config/recipes/base.rb
@@ -8,16 +8,12 @@ def set_default(name, *args, &block)
 end
 
 namespace :deploy do
-  desc "Install everything onto the server, + nokogirl dependancies + imagemagick libraries. Compile Imagemagick after the packages install."
+  desc "Install everything onto the server."
   task :install do
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install python-software-properties"
-    run "#{sudo} apt-get -y install libxslt-dev libxml2-dev libautotrace-dev libfreetype6-dev libgs-dev libjpeg62-dev liblqr-1-0-dev libpng12-dev libtiff4-dev libwmf-dev libx11-dev libxml++2.6-dev zlib1g-dev libperl-dev"
-    run "#{sudo} wget http://www.imagemagick.org/download/legacy/ImageMagick-6.7.7-10.tar.gz"
-    run "#{sudo} tar xvfz ImageMagick-6.7.7-10.tar.gz"
-    run "cd ImageMagick-6.7.7-10 && #{sudo} ./configure && #{sudo} make"
-    run "cd ImageMagick-6.7.7-10 && #{sudo} make install"
-    run "#{sudo} ldconfig /usr/local/lib"
-
+    run "#{sudo} apt-get -y install imagemagick"
+    run "#{sudo} apt-get -y install libxml2 libxslt-dev libxml2-dev libautotrace-dev libfreetype6-dev"  # Cause an error with nokoi girl
+    # I still need to install ffmpeg and dependancies!
   end
 end
